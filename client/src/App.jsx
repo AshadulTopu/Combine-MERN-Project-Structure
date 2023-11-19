8b2df5c0a7f67350160a872d06380ff8d07363e5
AshadulTopu <ashadulislam2003@gmail.com>
2023-11-19 22:07:37 +0600

    frontend and backend combination setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ const App = () => {
     <div>
       <h1 className="text-3xl font-bold">Combine MERN Project Structure</h1>
       <h1 className="text-3xl font-bold">React with vite and Tailwind</h1>
+      <h1 className="text-3xl font-bold">Vite + React + Tailwind</h1>
     </div>
   );
 };
