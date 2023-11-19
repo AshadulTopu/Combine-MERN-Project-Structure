bf821d4f2f6dad7d112b3297144a1ed9df9484da
AshadulTopu <ashadulislam2003@gmail.com>
2023-11-19 20:34:49 +0600

    combaine mern stack empty project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Combine MERN Project Structure</h1>
+      <h1 className="text-3xl font-bold">React with vite and Tailwind</h1>
+    </div>
+  );
+};
+
+export default App;
